ff479f5557d471f746f6bed2cfe13d93ce67eacd
Raul T. <raul.tomita@gmail.com>
2014-10-23 23:32:35 +0300

    Added skeleton

---
+++ Calculator/Calculator.Core/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Core
+{
+    public class Calculator
+    {
+        public double CurrentValue { get; set; }
+        public double Total { get; set; }
+        
+        public void Add()
+        {   
+        }
+
+        public void Subtruct()
+        {
+        }
+
+        public void Multiply()
+        {
+        }
+
+        public void Divide()
+        {
+        }
+
+
+    }
+}
